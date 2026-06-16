2526ceced3330a395b41101b665d8f573e84d4e8
gustavin002 <gustavomaia957@gmail.com>
2026-06-16 14:26:42 -0300

    inicio projeto

---
+++ src/main/java/com/sistema/monitoramento/ferramental/industrial/Application.java
@@ -0,0 +1,13 @@
+package com.sistema.monitoramento.ferramental.industrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
